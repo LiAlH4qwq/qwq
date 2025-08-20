dcd082a9e77afce33248854cac377157b2610133
LiAlH4 <lialh4qwq@outlook.com>
2025-08-20 20:13:49 +0800

    fix build script

--- build.ts
+++ build.ts
@@ -14,17 +14,28 @@ const targets = [
 
 type Main = () => Promise<never>
 type TargetNameToBunReadable = (target: string) => string
+type TargetNameToExecutableName = (target: string) => string
 
-const targetNameToBunReadable: TargetNameToBunReadable = (target) =>
-    target.endsWith("legacy") ?
+const targetNameToBunReadable: TargetNameToBunReadable = (target) => {
+    const part = target.endsWith("legacy") ?
         target.replaceAll("legacy", "baseline") :
         `${target}-modern`
+    return `bun-${part}`
+}
+
+const targetNameToExecutableName: TargetNameToExecutableName = (target) => {
+    const part = target.startsWith("darwin") ? `${target}.app` :
+        target.startsWith("windows") ? `${target}.exe` :
+            target
+    return `qwq-${part}`
+}
 
 const main: Main = async () => {
+    await Bun.$`mkdir target`
     await Promise.all(targets.map(async target =>
         await Bun.$`bun build --compile --minify --sourcemap \
-        --target=bun-${targetNameToBunReadable(target)} \
-        --outfile=target/qwq-${target} src/index.ts`))
+        --target=${targetNameToBunReadable(target)} \
+        --outfile=target/${targetNameToExecutableName(target)} src/index.ts`))
     process.exit()
 }
 
